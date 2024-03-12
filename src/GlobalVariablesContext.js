aa25698103285a2b117b1c77d310e449a0a4c2ae
LawrenceKurowski <lawrence.github@gmail.com>
2024-03-13 01:07:01 +0700

    corrected end time to adjust for time zone difference

--- src/GlobalVariablesContext.js
+++ src/GlobalVariablesContext.js
@@ -2,6 +2,6 @@
 
 // Define your global variables here
 export const startDate = new Date('2024-02-13T15:00:00').getTime();
-export const endDate = new Date('2024-03-13T09:30:00').getTime();
+export const endDate = new Date('2024-03-13T07:30:00').getTime();
 export const now = new Date().getTime();
 export const progress =  Math.min(Math.max((now - startDate)/(endDate - startDate)*100,0),100);
